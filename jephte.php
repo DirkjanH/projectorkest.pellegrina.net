7fdcbd8bbb4c8241ea30edec31e1e6984c878db8
Dirkjan Horringa <dirkjan@pellegrina.net>
2024-01-11 16:05:04 +0100

    Eerste commit

---
+++ jephte.php
@@ -0,0 +1,160 @@
+<!DOCTYPE HTML>
+<html><!-- InstanceBegin template="/Templates/LP_NL.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<head>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta charset="utf-8">
+
+   <!-- CSS: -->
+   <link rel="stylesheet" href="/css/pellegrina_stijlen.css" type="text/css">
+   <link rel="stylesheet" href="/css/pagina_stijlen.css" type="text/css">
+
+
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>De bijbelse opera Jephté</title>
+<!-- InstanceEndEditable -->
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/metatags+javascript.NL.php'; ?>
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/GA_code.php'; ?>
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+<!-- Facebook Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '537749209897328');
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=537749209897328&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+</head>
+<body>
+<div id="inhoud" class="w3-main">
+<?php 	
+	echo $navigatie;
+	echo '<span class="w3-opennav w3-xxlarge w3-hide-large" onclick="w3_open()">☰</span>';
+	require_once $_SERVER['DOCUMENT_ROOT'].'/includes/header.NL.php';
+	 ?>
+  <div id="main"> <!-- InstanceBeginEditable name="mainpage" -->
+  <h2>De bijbelse opera <em>Jephté</em></h2>
+  <p><em><strong>Jephté</strong></em> (Jefta) is een opera van de Franse componist Michel Pignolet de Montéclair. Het heeft de vorm van een <em><a href="https://nl.wikipedia.org/wiki/Tragédie_lyrique" target="_blank">tragédie en musique </a></em>in een proloog en vijf aktes (vanwege het onderwerp ervan werd het ook een <em>tragédie biblique</em> genoemd). Het libretto, door  abt Simon-Joseph Pellegrin, is gebaseerd op het bijbelverhaal van <a href="https://nl.wikipedia.org/wiki/Jefta" target="_blank">Jefta</a>, die beloofd de eerste die hij na een gewonnen veldslag ontmoet te offeren aan God en dan zijn blije dochter ontmoet. Het oratorium werd voor het eerst opgevoerd in de Académie royale de musique, Parijs op 28 februari 1732. Het was de eerste opera in Frankrijk die een verhaal uit de Bijbel gebruikte op het openbare toneel. Om deze reden verbood kardinaal De Noailles de uitvoering van het werk voor een tijdje. Montéclair maakte revisies voor hernemingen van het werk in maart 1732 en april 1737.</p>
+  <p>In 1735 werd de sopraan Catherine-Nicole Lemaure, die Iphise bij de première had gezongen, gedwongen om in de herneming van dat jaar te verschijnen op straffe van gevangenisstraf als ze dat niet zou doen. Daarom gaf ze opzettelijk een vreselijk slechte uitvoering om de productie te saboteren en werd ze uitgejouwd door het publiek. Ze werd 's nachts opgesloten in een Parijse gevangenis, tot ze de volgende avond terugkeerde om de rol correct te zingen.</p>
+  <h2>Michel Pignolet de Montéclair (1667 - 1737)</h2>
+  <p>Michel Pignolet de Montéclair werd geboren als Michel Pignolet in een eenvoudige weversfamilie in Andelot, Haute-Marne, Frankrijk. Pas later voegde hij &quot;Montéclair&quot; (de naam van een fort in zijn woonplaats) aan zijn naam toe. Er is weinig bekend over zijn leven en er zijn geen  portretten bekend. Toen hij negen was, werd hij toegelaten tot de koorschool , wat waarschijnlijk zijn enige kans om te ontsnappen aan de armoede van het leven van een wever was. In 1687 ging hij naar Parijs en werd hij aangenomen bij het orkest van de Opera, waar hij de 'base de violon' speelde. In Parijs studeerde hij bij Jean-Baptiste Moreau.</p>
+  <p>Op een gegeven moment tussen 1687 en de vroege jaren van de nieuwe eeuw, is hij waarschijnlijk <em>maître de musique </em>geweest van de Prins van Vaudémont en heeft hij hem gevolgd naar Italië. Waarschijnlijk kreeg hij daar het idee om de contrabas toe te voegen aan het opera-orkest.</p>
+  <div class="fotorechts"> <img src="../../Images/ornaments1.png" class="w3-image geenlijn" width="350" height="286" alt="D'Anglebert"/><br>
+     D'Angleberts versieringentabel</div>
+  <p>Zijn hele leven moet Montéclair hebben gewerkt als muziekleraar van hoog aanzien: onder zijn leerlingen waren de dochters van zijn collega François Couperin. Zijn benadering van lesgeven was fris en bijna modern. Hij publiceerde boeken over het onderwijzen van muziek (bijvoorbeeld 1709), en rond 1730 publiceerde hij de bundel <em>Recueil de brunettes, </em>die vocale muziek gearrangeerd voor fluit bevat. De collectie was uitdrukkelijk bedoeld als een pedagogisch hulpmiddel om de Franse stijl te leren, en om die reden bevat de muzieknotatie ook de tekst. Hij opende een muziekwinkel in 1721, stopte met lesgeven in 1735 en gaf zijn positie in het operarorkest op kort voor zijn dood. Hij stierf in 1737 in Aumont.</p>
+  <p>Montéclair was niet bijzonder productief als componist, maar was een vernieuwer in orkestratie die een belangrijke invloed had op de ontwikkeling van de kunstvorm. Zijn werk werd later overgenomen door Jean-Philippe Rameau. Zijn specialiteit was het gebruik van bepaalde instrumenten om de uitbeelding van de toneelscène te verbeteren, door bijvoorbeeld  hoorns achter het podium zacht te laten spelen om een ​​verre jacht te simuleren.</p>
+  <h2>Rollen in de opera</h2>
+  <p><em>Jephté</em>  heeft grote, maar ook veel kleinere rollen, die in principe worden gezongen door zangers die deelnemen aan de cursus:</p>
+  <table class="w3-table-all">
+     <tbody>
+        <tr>
+           <th>Rol</th>
+           <th>Stemtype</th>
+           <th>Bij de première gezongen door</th>
+        </tr>
+        <tr>
+           <td>Jephté</td>
+           <td>bas</td>
+           <td>Claude-Louis-Dominique Chassé de Chinais</td>
+        </tr>
+        <tr>
+           <td>Iphise</td>
+           <td>sopraan</td>
+           <td>Catherine-Nicole Le Maure</td>
+        </tr>
+        <tr>
+           <td>Ammon</td>
+           <td>haute-contre (hoge tenor of alt)</td>
+           <td>Denis-François Tribou</td>
+        </tr>
+        <tr>
+           <td>Phinée</td>
+           <td>bas</td>
+           <td>Jean Dun</td>
+        </tr>
+        <tr>
+           <td>Abdon</td>
+           <td>haute-contre (hoge tenor of alt)</td>
+           <td></td>
+        </tr>
+        <tr>
+           <td>Almasie</td>
+           <td>sopraan</td>
+           <td>Marie Antier</td>
+        </tr>
+        <tr>
+           <td>Abner</td>
+           <td>bas</td>
+           <td></td>
+        </tr>
+        <tr>
+           <td>Élise</td>
+           <td>sopraan</td>
+           <td></td>
+        </tr>
+        <tr>
+           <td>Apollon</td>
+           <td>bas</td>
+           <td></td>
+        </tr>
+        <tr>
+           <td>Polyhymnie</td>
+           <td>(hoge) sopraan</td>
+           <td></td>
+        </tr>
+        <tr>
+           <td>Terpsichore</td>
+           <td>(mezzo?) sopraan</td>
+           <td></td>
+        </tr>
+        <tr>
+           <td>Venus</td>
+           <td>(mezzo?) sopraan</td>
+           <td></td>
+        </tr>
+        <tr>
+           <td>La Verité</td>
+           <td>sopraan</td>
+           <td></td>
+        </tr>
+     </tbody>
+  </table>
+  
+  <h3><span id="Synopsis">Samenvatting van het libretto</span></h3>
+  <div class="fotorechts"> <img src="/Images/jephtetragedie.jpg" class="w3-image" width="500" height="778" alt=""/><br>
+ eerste muziekpagina van de partituur van <em>Jephté</em>  </div>
+<h4><span id="Prologue">Proloog</span></h4>
+  <p><em>La Verité </em>(de waarheid) jaagt de valse heidense goden  Apollo, Venus en Polyhymnia weg.</p>
+  <h4>Akte een</h4>
+  <p>De hogepriester Phinée kiest Jefta als leider van de Israëlieten ,terwijl diezich voorbereiden om de Efraïmieten, de mensen van Efraïm, aan te vallen. Jefta belooft God de eerste persoon te offeren die hij ziet bij zijn terugkeer uit de strijd, als hij de overwinning behaalt.</p>
+  <h4>Akte twee</h4>
+  <p>De leider van de Efraïmieten, Ammon, wordt gevangen gehouden in het paleis van Jefta. Ammon negeert de aandrang van zijn metgezel Abner om te ontsnappen, omdat hij verliefd is geworden op Jefta's dochter, Iphise. Iphise bekent  haar moeder dat ze ook verliefd is op Ammon. Nieuws arriveert van Jefta's overwinning in de strijd.</p>
+  <h4>Akte drie</h4>
+  <p>Jefta is geschokt als Iphise de eerste persoon is die hij ziet wanneer hij thuiskomt. Hij vertelt haar over zijn gelofte en zij bereidt zich voor om te worden opgeofferd, ondanks de smeekbeden van Ammon.</p>
+  <h4>Akte vier</h4>
+  <p>Iphise klaagt over haar lot, maar accepteert het. Ammon zweert dat hij zijn leger zal leiden om haar te redden, maar ze verwerpt zijn aanbod.</p>
+  <h4>Akte vijf</h4>
+  <p>De Israëlieten bereiden het offer in de tempel voor. Ammon en zijn mannen komen binnen maar ze worden getroffen door een bliksemschicht uit de hemel. De priester Phinéé verklaart dat God tevreden is met Iphise en dat haar leven wordt gespaard.</p>
+  <h3>Bladmuziek</h3>
+  <p>De partituur van <em>Jephté</em> is bewaard gebleven in een gedrukte editie uit het jaar 1732, die beschikbaar is op <a href="https://imslp.org/wiki/Jepht%C3%A9_(Mont%C3%A9clair%2C_Michel_Pignolet_de)" target="_blank">IMSLP</a>. Hij werd gedrukt en gepubliceerd door François Boivin, een neef van Montéclair. De totale duur van de muziek is ongeveer 150 minuten, dus voor dit project zullen we een verkorte versie maken van ongeveer een uur  muziek, een selectie die zich concentreert op de aria's en koren. Een collectief van docenten en deelnemers transcribeert de partituur in moderne notatie en maakt makkelijk leesbare partijen.   </p>
+  <div class="w3-panel container">
+     <div class="fotolinks"><a href="https://open.spotify.com/album/4HR0Ke8ejlEFQSvAQgzuAS?si=Zh_6uMf2StWmS6TOuQCfVQ" target="_blank"><img src="../../Images/CD_Jephté.jpg" class="w3-image" width="350" height="302" alt="CD cover Jephté"></a></div>
+     <h2>Opname op CD</h2>
+     <p><em>Jephté </em>werd op CD gezet door Les Arts Florissants o.l.v. William Christie (Harmonia Mundi, 1992), met Jacques Bona, Sophie Daneman, Nicolas Rivenq, Claire Brua en Mark Padmore. Deze opname is <a href="https://open.spotify.com/album/4HR0Ke8ejlEFQSvAQgzuAS?si=Zh_6uMf2StWmS6TOuQCfVQ" target="_blank">beschikbaar op Spotify</a>.</p>
+  </div>
+  <!-- InstanceEndEditable -->
+    <h2><a href="javascript: history.go(-1)">Terug</a></h2>
+  </div>
+</div>
+</body>
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/includes/footer.php'; ?>
+<!-- InstanceEnd --></html>
---
+++ reactie_dvorak_2019.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<title>Reacties project Dvořák</title>
+	<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
+	<link href="bestellen/css/PO.css" rel="stylesheet" type="text/css">
+	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+	<div class="w3-content w3-white">
+		<header class="w3-center">
+			<h2>Herhalingsconcert <i><a href="https://www.pellegrina.net" target="_blank">La
+      Pellegrina</a></i> zomercursus </h2>
+			<h1 class="w3-purple">Dvořák-project 2019</h1>
+			<p>zaterdag 9 februari 2019 • Rehobothkerk • Briljantlaan 6 • Utrecht</p>
+		</header>
+		<div class="w3-panel w3-card-4">
+			<h3><strong><em>La Pellegrina</em> projectkoor en -orkest <br>
+			</strong>Dirkjan Horringa <em>dirigent </em>, m.m.v. Anitra Jellema <em>sopraan</em> en Mitchell Sandler <em>bas</em>
+			</h3>
+			<p>Op zaterdagavond 9 februari voerden solisten, koor en orkest van <em>La Pellegrina</em> in Utrecht een programma met werken van Dvořák <em></em> uit:</p>
+			<ul>
+				<li>Vodník (De Waterman) op. 107 </li>
+				<li>Zes Moravische Duetten (arr. Leoš Janáček &amp; Dirkjan Horringa)</li>
+				<li>Te Deum op. 103</li>
+			</ul>
+		</div>
+		<div class="w3-panel w3-card-4">
+			<h3>Reactie Marileen van der Most van Spijk, sopraan in het koor</h3>
+			<p><em>Zij schreef:</em><br> Heel veel dank voor de organisatie van dit project. Daar komt veel bij kijken! Jullie hadden het zo zorgzaam voorbereid en verzorgd. Alle mails met de deelnemers, het zoeken van vervangers, de repetitiedagen in de kerk, het organiseren van de pizza’s, het prachtige programmaboekje. Hopelijk is dit voor jullie ook de moeite waard geweest. Ik heb in elk geval erg genoten van het meedoen aan dit project. Nu niet in het orkest, maar in het koor. Het was een voorrecht om onder Mitchell de muziek in te studeren. Heel stimulerend. Willen jullie hem ook bedanken voor de drie zaterdagen, waarin hij ons op zo positieve en professionele wijze begeleidde. Ik heb zijn mailadres helaas niet. Hij heeft zelf ook zo prachtig gezongen als solist! En dan die drie Tsjechische musici die speciaal voor het concert heen en weer kwamen rijden. Ongelooflijk.
+			</p>
+		</div>
+		<div class="w3-panel w3-card-4">
+			<h3>Reactie & foto's Robert van der Linde, bas in het koor</h3>
+			<p><em>Hij schreef:</em><br> Als tijdelijke koordeelnemer aan dit project kijk ik met genoegen terug op dit 'avontuur'. Ik kon vooraf moeilijk inschatten of het zou lukken in een relatief korte tijd voldoende met deze muziek vertrouwd te raken, zeker met het gebruik van de tsjechische taal. Het was inderdaad flink 'aanpoten', maar de meest waardevolle ervaring lag in de repetities met Mitchell, een fantastische koordirigent.<br> Ik denk ik zo ongeveer de enige ben geweest die af en toe van het gebeuren een foto heeft kunnen maken. Ik wil je deze niet onthouden; misschien zijn ze nog ergens voor te gebruiken (ten behoeve van de eigen leden?).</p>
+			<img src="images/Dvorak 2019/20190202_135924.jpg" class="geenlijn w3-image w3-panel w3-card" alt=""/>
+			<img src="images/Dvorak 2019/20190203_120021(b).jpg" class="geenlijn w3-image w3-panel w3-card" alt=""/>
+			<img src="images/Dvorak 2019/20190203_120154(b).jpg" class="geenlijn w3-image w3-panel w3-card" alt=""/>
+			<img src="images/Dvorak 2019/20190209_143158(b).jpg" class="geenlijn w3-image w3-panel w3-card" alt=""/>
+			<img src="images/Dvorak 2019/20190209_221327(b).jpg" class="geenlijn w3-image w3-panel w3-card" alt=""/>
+		</div>
+	</div>
+</body>
+</html>
